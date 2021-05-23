9977831c314f2bdbdb9179629b27ddccca4f0611
schofija <schofija@oregonstate.edu>
2021-05-22 22:27:03 -0700

    Added unittest functionality

--- test_leapYear.py
+++ test_leapYear.py
@@ -8,15 +8,26 @@ import unittest
 #----------------------------
 
 #pytest functions
-def test_leapYear1():
+def test_leapYear1_py():
     assert leapYear.isLeapYear(2000) == 1
 
-def test_leapYear2():
+def test_leapYear2_py():
     assert leapYear.isLeapYear(2001) == 0
     
-def test_leapYear3():
+def test_leapYear3_py():
     assert leapYear.isLeapYear(1900) == 0
     
-def test_leapYear4():
+def test_leapYear4_py():
     assert leapYear.isLeapYear(2004) == 1
+    
+def test_leapYear5_py():
+    assert leapYear.isLeapYear("asdf") == 2
+    
+#unittest
+class test_LeapYear(unittest.TestCase):
+    def test_leapYear6_ut(self):
+        self.assertTrue(leapYear.isLeapYear("2000"), 2)
+        
+    def test_leapYear7_ut(unittest.TestCase):
+        self.assertTrue(leapYear.isLeapYear(1800), 0)
 
